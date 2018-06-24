197d0e5ab9c92b4c91a87d4c9fc221d850aed2f2
Gary Leeming <gary.leeming@gmail.com>
2018-06-24 12:54:47 +0100

    Python SCripts
    Collected scripts for kids to play with Minecraft on Pi

---
+++ 02_getpos.py
@@ -0,0 +1,5 @@
+//get position
+x, y, z = mc.player.getPos()
+
+//teleport
+mc.player.setPos(x, y+100, z)
---
+++ 03_setblock.py
@@ -0,0 +1,10 @@
+from mcpi import block
+
+/set single block
+x, y, z = mc.player.getPos()
+mc.setBlock(x+3, y, z, block.STONE.id)
+
+//set multiple blocks
+stone = 1
+x, y, z = mc.player.getPos()
+mc.setBlocks(x+1, y+1, z+1, x+11, y+11, z+11, stone)
